2b7f2044549b20a8747b5cab5f6bdcbf4dd2dfce
sihc <tvquocchi@gmail.com>
2018-05-11 16:52:59 +0700

    finish login

--- API/loginHandle.php
+++ API/loginHandle.php
@@ -4,38 +4,39 @@
 	$requestData = json_decode(file_get_contents('php://input'), true);
 
 	//	Make object return
-	$return = {}
+	$return = [];
 
 	//	Get param
-	$username = $requestData('username');
-	$password = $requestData('password');
+	$username = $requestData['username'];
+	$password = md5($requestData['password']);
 	$return['username']= $username;
 	$return['password']= $password;
 
 	if( $username == "" || $password == "" ){
 		$result['message'] = 'Require username and password!';
-		echo "result";
+		echo json_encode((object)$return);
 		http_response_code(404);
 	}
 
 	//	Connect
 	$connector = mysqli_connect('localhost', 'root', '') or die('Could not connect: '.mysql_error());
-	mysqli_set_charset($connect, 'utf8');
-	$db_selected = mysqli_select_db($connector, 'user');
+	mysqli_set_charset($connector, 'utf8');
+	$db_selected = mysqli_select_db($connector, 'simpleonlinequiz');
 
 	//	Query
 	$query = "SELECT * FROM `user` WHERE username = '".$username."' AND password = '".$password."';";
+	$return['query'] = $query;
 	$result = mysqli_query($connector, $query);
 
 	if( $result ){
-		if(mysqli_num_rows($result) == 1){
+		if( mysqli_num_rows($result) == 1 ){
 			$_SESSION['user'] = $username;
 			$return['message'] = 'success';
 			http_response_code(200);
 		}
 		else {
-			$return['message'] = echo 'Invalid username or password!';
-			http_response_code(404);
+			$return['message'] = 'Invalid username or password!';
+			http_response_code(400);
 		}
 		mysqli_free_result($result);
 	}
@@ -43,5 +44,5 @@
 		$return['message'] = 'Login error!!!';
 		http_response_code(500);
 	}
-	echo json_encode($return);
+	echo json_encode((object)$return);
 ?>
