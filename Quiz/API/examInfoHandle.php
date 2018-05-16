ef7f67c74e2f63d9f0f279ef1ba11d343942e727
sihc <tvquocchi@gmail.com>
2018-05-16 15:16:49 +0700

    fix examInfoHandle done

--- Quiz/API/examInfoHandle.php
+++ Quiz/API/examInfoHandle.php
@@ -1,33 +1,45 @@
 <?php
 	session_start();
 	header('Content-type: application/json');
-
 	//	Make object return
 	$return = [];
-
+	if(!isset($_SESSION['user'])){
+		$return['message']= 'Invalid user session!';
+		echo json_encode((object)$return);
+		http_response_code(400);
+	}
+	else
 	try {
+		$username = $_SESSION['user'];
 		//	Connect
 		$connector = mysqli_connect('localhost', 'root', '') or die('Could not connect: '.mysql_error());
 		mysqli_set_charset($connector, 'utf8');
 		$db_selected = mysqli_select_db($connector, 'simpleonlinequiz');
-
 		//	Query
-		$query = "SELECT * FROM `contest` WHERE username = '".$username."' AND password = '".$password."';";
-		// $return['query'] = $query;
+		$query = "SELECT * FROM `userhistory` INNER JOIN `contest` ON contest.ID = ContestID WHERE username= '".$username."';";
+		$return['query'] = $query;
 		$result = mysqli_query($connector, $query);
-
 		if( $result ){
-			if( mysqli_num_rows($result) == 1 ){
-				$right = mysqli_fetch_array($result, MYSQLI_BOTH);
-				$_SESSION['user'] = $username;
-				$_SESSION['role'] = $right['role'];
-				$return['user'] = $username;
-				$return['role'] = $right['role'];
+			if( mysqli_num_rows($result) > 0 ){
+				$cnt= 0;
+				$contentArr= [];
+				while($dataRow = mysqli_fetch_array($result, MYSQLI_BOTH)){
+					// $return["".$cnt] = $dataRow;
+					$rowArr = [];
+					$rowArr['id']= $dataRow['historyID'];
+					$rowArr['name']= $dataRow['contestname'];
+					$rowArr['teacher']= $dataRow['teacher'];
+					$rowArr['yourpoint']= $dataRow['Mark'];
+					// $rowArr['id']= $dataRow['id'];
+					$contentArr[$cnt] = $rowArr;
+					$cnt += 1;
+				}
+				$return['content'] = $contentArr;
 				$return['message'] = 'success';
 				http_response_code(200);
 			}
 			else {
-				$return['message'] = 'Invalid username or password!';
+				$return['message'] = 'Invalid username in session!';
 				http_response_code(400);
 			}
 			mysqli_free_result($result);
