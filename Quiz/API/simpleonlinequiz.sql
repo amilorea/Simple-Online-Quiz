32cb59cd4091048bfe9eee5bb2e6079cc360b9f2
sihc <tvquocchi@gmail.com>
2018-05-16 12:19:30 +0700

    fix examInfoHandle done

--- Quiz/API/simpleonlinequiz.sql
+++ Quiz/API/simpleonlinequiz.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th5 13, 2018 lúc 11:42 AM
+-- Thời gian đã tạo: Th5 16, 2018 lúc 07:18 AM
 -- Phiên bản máy phục vụ: 10.1.31-MariaDB
 -- Phiên bản PHP: 7.2.3
 
@@ -25,10 +25,10 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `constest`
+-- Cấu trúc bảng cho bảng `contest`
 --
 
-CREATE TABLE `constest` (
+CREATE TABLE `contest` (
   `ID` int(11) NOT NULL,
   `contestname` text COLLATE utf8_unicode_ci NOT NULL,
   `teacher` text COLLATE utf8_unicode_ci NOT NULL,
@@ -36,10 +36,10 @@ CREATE TABLE `constest` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Đang đổ dữ liệu cho bảng `constest`
+-- Đang đổ dữ liệu cho bảng `contest`
 --
 
-INSERT INTO `constest` (`ID`, `contestname`, `teacher`, `created`) VALUES
+INSERT INTO `contest` (`ID`, `contestname`, `teacher`, `created`) VALUES
 (1, 'Midterm', 'sihcpro', '2018-05-12 18:01:29'),
 (2, 'Final', 'equal1', '2018-05-12 18:01:29');
 
@@ -66,7 +66,9 @@ CREATE TABLE `question` (
 --
 
 INSERT INTO `question` (`ID`, `contest`, `question`, `A`, `B`, `C`, `D`, `correct`, `point`) VALUES
-(1, 1, 'What is that?', 'That is what?', 'What that is?', 'Is that what?', 'Is that, What?', 'A', 2.75);
+(1, 1, 'What is that?', 'That is what?', 'What that is?', 'Is that what?', 'Is that, What?', 'A', 2.75),
+(2, 2, 'Does Bang free?', 'Yes, He is.', 'Yes, he does.', 'No, he doesn\'t', 'No, he didn\'t.', 'B', 2.25),
+(3, 2, 'Do you like English?', 'Yes, I\'m.', 'Yes, I\'ll.', 'No, I didn\'t.', 'No never!', 'D', 3.25);
 
 -- --------------------------------------------------------
 
@@ -96,7 +98,7 @@ INSERT INTO `role` (`ID`, `rolename`) VALUES
 --
 
 CREATE TABLE `user` (
-  `username` text COLLATE utf8_unicode_ci NOT NULL,
+  `username` varchar(40) COLLATE utf8_unicode_ci NOT NULL,
   `accountname` text COLLATE utf8_unicode_ci NOT NULL,
   `password` text COLLATE utf8_unicode_ci NOT NULL,
   `role` int(11) NOT NULL DEFAULT '1'
@@ -113,14 +115,39 @@ INSERT INTO `user` (`username`, `accountname`, `password`, `role`) VALUES
 ('sihc', 'Chi', '3d186804534370c3c817db0563f0e461', 3),
 ('sihcpro', 'Chi dep trai', '3d186804534370c3c817db0563f0e461', 2);
 
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `userhistory`
+--
+
+CREATE TABLE `userhistory` (
+  `historyID` int(11) NOT NULL,
+  `username` varchar(40) COLLATE utf8_unicode_ci NOT NULL,
+  `ContestID` int(11) NOT NULL,
+  `Mark` float NOT NULL DEFAULT '0',
+  `Time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `userhistory`
+--
+
+INSERT INTO `userhistory` (`historyID`, `username`, `ContestID`, `Mark`, `Time`) VALUES
+(1, 'equal', 1, 7, '2018-05-16 10:56:38'),
+(2, 'equal', 2, 8, '2018-05-16 10:56:38'),
+(3, 'sihc', 1, 7, '2018-05-16 10:57:07'),
+(4, 'sihc', 1, 9, '2018-05-16 10:57:18'),
+(5, 'sihc', 2, 8, '2018-05-16 10:57:29');
+
 --
 -- Chỉ mục cho các bảng đã đổ
 --
 
 --
--- Chỉ mục cho bảng `constest`
+-- Chỉ mục cho bảng `contest`
 --
-ALTER TABLE `constest`
+ALTER TABLE `contest`
   ADD PRIMARY KEY (`ID`);
 
 --
@@ -140,24 +167,38 @@ ALTER TABLE `role`
 -- Chỉ mục cho bảng `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`username`(40)),
+  ADD PRIMARY KEY (`username`),
   ADD KEY `userrole` (`role`);
+
+--
+-- Chỉ mục cho bảng `userhistory`
+--
+ALTER TABLE `userhistory`
+  ADD PRIMARY KEY (`historyID`),
+  ADD KEY `user` (`username`),
+  ADD KEY `contest` (`ContestID`);
 
 --
 -- AUTO_INCREMENT cho các bảng đã đổ
 --
 
 --
--- AUTO_INCREMENT cho bảng `constest`
+-- AUTO_INCREMENT cho bảng `contest`
 --
-ALTER TABLE `constest`
+ALTER TABLE `contest`
   MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT cho bảng `question`
 --
 ALTER TABLE `question`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT cho bảng `userhistory`
+--
+ALTER TABLE `userhistory`
+  MODIFY `historyID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Các ràng buộc cho các bảng đã đổ
@@ -167,13 +208,20 @@ ALTER TABLE `question`
 -- Các ràng buộc cho bảng `question`
 --
 ALTER TABLE `question`
-  ADD CONSTRAINT `belongToContest` FOREIGN KEY (`contest`) REFERENCES `constest` (`ID`);
+  ADD CONSTRAINT `belongToContest` FOREIGN KEY (`contest`) REFERENCES `contest` (`ID`);
 
 --
 -- Các ràng buộc cho bảng `user`
 --
 ALTER TABLE `user`
   ADD CONSTRAINT `userrole` FOREIGN KEY (`role`) REFERENCES `role` (`ID`);
+
+--
+-- Các ràng buộc cho bảng `userhistory`
+--
+ALTER TABLE `userhistory`
+  ADD CONSTRAINT `contest` FOREIGN KEY (`ContestID`) REFERENCES `contest` (`ID`),
+  ADD CONSTRAINT `user` FOREIGN KEY (`username`) REFERENCES `user` (`username`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
