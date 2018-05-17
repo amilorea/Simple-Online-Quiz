353be4972b63a57ca6f9e14f6818bc31140fa1a5
sihc <tvquocchi@gmail.com>
2018-05-17 09:17:09 +0700

    add examHandle and examSearch

---
+++ Quiz/API/examHandle.php
@@ -0,0 +1,72 @@
+<?php
+	session_start();
+	header('Content-type: application/json');
+	$requestData = json_decode(file_get_contents('php://input'), true);
+
+	//	Make object return
+	$return = [];
+	// if(!isset($_SESSION['user'])){
+	// 	$return['message']= 'Invalid user session!';
+	// 	echo json_encode((object)$return);
+	// 	http_response_code(400);
+	// }
+	// else
+	try {
+		// $username = $_SESSION['user'];
+		//	Get params
+		$contestID = $requestData['id'];
+
+		//	Connect
+		$connector = mysqli_connect('localhost', 'root', '') or die('Could not connect: '.mysql_error());
+		mysqli_set_charset($connector, 'utf8');
+		$db_selected = mysqli_select_db($connector, 'simpleonlinequiz');
+
+		//	Query
+		$query1 = "SELECT contest.*, accountname FROM `contest` INNER JOIN `user` ON user.username = teacher WHERE ID = '".$contestID."';";
+		$query2 = "SELECT * FROM `question` WHERE contestID = '".$contestID."';";
+		$return['query1'] = $query1;
+		$return['query2'] = $query2;
+		$resultContest = mysqli_query($connector, $query1);
+		$result = mysqli_query($connector, $query2);
+		if( $result && $resultContest ){
+			if( mysqli_num_rows($result)*mysqli_num_rows($resultContest) > 0 ){
+				$contestData = mysqli_fetch_array($resultContest, MYSQLI_BOTH);
+				$return['name'] = $contestData['contestname'];
+				$return['teacher'] = $contestData['accountname'];
+
+				$cnt= 0;
+				$contentArr= [];
+				while($dataRow = mysqli_fetch_array($result, MYSQLI_BOTH)){
+					// $return["".$cnt] = $dataRow;
+					$rowArr = [];
+					$rowArr['id']= $dataRow['questionID'];
+					$rowArr['score']= $dataRow['point'];
+					$rowArr['content']= $dataRow['question'];
+					$rowArr['anwser1']= $dataRow['A'];
+					$rowArr['anwser2']= $dataRow['B'];
+					$rowArr['anwser3']= $dataRow['C'];
+					$rowArr['anwser4']= $dataRow['D'];
+					// $rowArr['id']= $dataRow['id'];
+					$contentArr[$cnt] = $rowArr;
+					$cnt += 1;
+				}
+				$return['content'] = $contentArr;
+				$return['message'] = 'success';
+				http_response_code(200);
+			}
+			else {
+				$return['message'] = 'Contest is empty!';
+				http_response_code(400);
+			}
+			mysqli_free_result($result);
+		}
+		else {
+			$return['message'] = 'Server error!!!';
+			http_response_code(500);
+		}
+		echo json_encode((object)$return);
+	} catch ( Exception $error ) {
+		echo json_encode((object)$return);
+		http_response_code(400);
+	}
+?>
--- Quiz/API/examSearchHandle.php
+++ Quiz/API/examSearchHandle.php
@@ -1,22 +1,21 @@
 <?php
 	session_start();
 	header('Content-type: application/json');
+	$requestData = json_decode(file_get_contents('php://input'), true);
+
 	//	Make object return
 	$return = [];
-	if(!isset($_SESSION['user'])){
-		$return['message']= 'Invalid user session!';
-		echo json_encode((object)$return);
-		http_response_code(400);
-	}
-	else
+
 	try {
-		$username = $_SESSION['user'];
+		//	Get params
+		$return['params'] = $requestData;
+		$searchName = $requestData['name'];
 		//	Connect
 		$connector = mysqli_connect('localhost', 'root', '') or die('Could not connect: '.mysql_error());
 		mysqli_set_charset($connector, 'utf8');
 		$db_selected = mysqli_select_db($connector, 'simpleonlinequiz');
 		//	Query
-		$query = "SELECT * FROM `userhistory` INNER JOIN `contest` ON contest.ID = contestID WHERE username= '".$username."';";
+		$query = "SELECT ID, contestname, teacher, created, COUNT( question.questionID ) as count, SUM(question.point) as total FROM `contest` LEFT OUTER JOIN `question` ON question.`contestID` = contest.ID WHERE contestname LIKE '%".$searchName."%' GROUP BY contest.ID";
 		$return['query'] = $query;
 		$result = mysqli_query($connector, $query);
 		if( $result ){
@@ -24,12 +23,12 @@
 				$cnt= 0;
 				$contentArr= [];
 				while($dataRow = mysqli_fetch_array($result, MYSQLI_BOTH)){
-					// $return["".$cnt] = $dataRow;
 					$rowArr = [];
-					$rowArr['id']= $dataRow['historyID'];
+					$rowArr['id']= $dataRow['ID'];
 					$rowArr['name']= $dataRow['contestname'];
 					$rowArr['teacher']= $dataRow['teacher'];
-					$rowArr['yourpoint']= $dataRow['Mark'];
+					$rowArr['totalpoint']= $dataRow['total'];
+					$rowArr['questioncount']= $dataRow['count'];
 					// $rowArr['id']= $dataRow['id'];
 					$contentArr[$cnt] = $rowArr;
 					$cnt += 1;
@@ -39,7 +38,7 @@
 				http_response_code(200);
 			}
 			else {
-				$return['message'] = 'Your history is empty!';
+				$return['message'] = 'There are no Contest!';
 				http_response_code(400);
 			}
 			mysqli_free_result($result);
